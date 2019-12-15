4868e0232965e01fe6482bd9e470f4b54ef59783
m <m@example.org>
2019-12-15 12:24:13 +0000

    merged old script into find parameters

--- WorkEnv/PathScripts/cleandirectory.sh
+++ WorkEnv/PathScripts/cleandirectory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #remove files we don't we don't care about in a git directory
-find ./ -type f -a \( -iname "*.bak" -o -iname "*~" -o -iname ".directory" \) -exec  rm {} \;
+find ./ -type f -a \( -iname "*.bak" -o -iname "*~" -o -iname ".directory" -o -iname "*.p[ml].tdy" -o -iname "*.kate-swp"   \) -exec  rm {} \;
 #remove empty directories, -depth goes to the end of each path first
 find ./ -depth -empty -delete
 find ./ -depth -empty -delete
