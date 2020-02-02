572ac6b42bae31b30a7e188777993cd4bf60b075
m <mmacnair@cpan.org>
2020-02-02 09:30:07 +0000

    new tool

---
+++ WorkEnv/PathScripts/perl2json.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use Carp qw/croak confess cluck/;
+use JSON;
+main(@ARGV);
+
+sub main {
+	my ($path) = @_;
+	die unless -f $path;
+	open( my $fh, '<:raw', $path )
+	or die "failed to open file [$path] : $!";
+
+	my $buffer;
+	while ( my $line = <$fh> ) {
+		chomp( $line );
+		$buffer .= $line;
+	}
+	close( $fh );
+	my $json = JSON->new()->canonical(1)->pretty(1);
+	my $v = eval($buffer);
+	print $json->encode($v) , $/;
+
+}
+ 
