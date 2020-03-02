3c726eef4167d9b3cf2d149f86a227720707b3c6
m <mmacnair.org>
2020-03-02 02:48:53 +0000

    new file

--- WorkEnv/Bash/bash_source.sh
+++ WorkEnv/Bash/bash_source.sh
@@ -1,6 +1,6 @@
-export TOOLBOXDIR="/mnt/sda3/home/m/git/Toolbox"
-export PATH="$PATH:/mnt/sda3/home/m/git/Toolbox/WorkEnv//PathScripts"
-export PERL5LIB="$PERL5LIB:/mnt/sda3/home/m/git/Toolbox/perl/lib/"
+export TOOLBOXDIR="/mnt/store/home/m/gits/Toolbox"
+export PATH="$PATH:/mnt/store/home/m/gits/Toolbox/WorkEnv//PathScripts"
+export PERL5LIB="$PERL5LIB:/mnt/store/home/m/gits/Toolbox/perl/lib/"
 # straight string dumped into bash_source.rc when setup.pl is run
 # setup.pl provides the environment variables to make this work
 source $TOOLBOXDIR/WorkEnv/Bash/aliases.sh
