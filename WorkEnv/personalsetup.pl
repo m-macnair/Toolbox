709731f5fd73a56a891358bbe763a7d9b48a3b38
m <mmacnair@cpan.org>
2020-03-02 02:24:06 +0000

    script correction

--- WorkEnv/personalsetup.pl
+++ WorkEnv/personalsetup.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+# for when i'm me and not someone else
 `git config --global core.editor "vi "`;
 `git config --global user.email  mmacnair@cpan.org`;
 `git config --global user.name "m"`;
