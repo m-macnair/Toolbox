bfbcab6ac736ee3f14e54d6a5eb6b56477ec8a48
m <mmacnair.org>
2020-03-03 06:54:53 +0000

    remove bash source and keep it that way lets hope

--- WorkEnv/Bash/bash_source.sh
+++
@@ -1,7 +0,0 @@
-export TOOLBOXDIR="/mnt/sda3/home/m/git/Toolbox"
-export PATH="$PATH:/mnt/sda3/home/m/git/Toolbox/WorkEnv//PathScripts"
-export PERL5LIB="$PERL5LIB:/mnt/sda3/home/m/git/Toolbox/perl/lib/"
-# straight string dumped into bash_source.rc when setup.pl is run
-# setup.pl provides the environment variables to make this work
-source $TOOLBOXDIR/WorkEnv/Bash/aliases.sh
-source $TOOLBOXDIR/WorkEnv/Bash/functions.sh
