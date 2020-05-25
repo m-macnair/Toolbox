063324d54f46bdc16fa8184e7652c55b205a4e6b
m <mmacnair@cpan.org>
2020-05-25 10:27:57 +0100

    extended basic.sh

--- systems/debian/setup/basic.sh
+++ systems/debian/setup/basic.sh
@@ -1,8 +1,13 @@
 apt-get update
 apt-get upgrade
 
+apt-get install --assume-yes screen >> apt_install.log 
+screen
+
 #get the perl going
 apt-get install --assume-yes make cpanminus
-apt-get install --assume-yes iptables git net-tools  screen htop sudo  >> apt_install.log 
-screen
+apt-get install --assume-yes iptables git net-tools  htop sudo  >> apt_install.log 
+git clone https://github.com/m-macnair/Toolbox.git
+
+ip address
 
