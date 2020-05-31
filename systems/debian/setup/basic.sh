a7ed62a335ff59c487e0fd895c6b0f6a14b83f50
mmacnair <mark@traveltek.net>
2020-05-31 11:30:12 +0100

    yes

--- systems/debian/setup/basic.sh
+++ systems/debian/setup/basic.sh
@@ -3,6 +3,7 @@ apt-get upgrade
 
 #get the perl going
 apt-get install --assume-yes make cpanminus
-apt-get install --assume-yes iptables git net-tools  screen htop sudo  >> apt_install.log 
+apt-get install --assume-yes iptables git net-tools  screen htop sudo sysvinit-utils  >> apt_install.log 
+
 screen
 
