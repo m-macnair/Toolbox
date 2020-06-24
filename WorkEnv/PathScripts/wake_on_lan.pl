428bbbd751692d9bca471b77de3f03db5db6cd5d
mmacnair <mark@traveltek.net>
2020-06-24 21:12:37 +0100

    new tool from 9 years ago

---
+++ WorkEnv/PathScripts/wake_on_lan.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use Net::Wake;
+main(@ARGV);
+
+sub main {
+
+	my ($udpstr) = @_;
+	if ($udpstr) {
+		my @udps = split ( ',', $udpstr );
+		for my $udp (@udps) {
+			print "Waking $udp$/";
+			Net::Wake::by_udp( undef, $udp );
+		}
+	} else {
+		print "No udpstring provided$/" unless $udpstr;
+	}
+
+}
