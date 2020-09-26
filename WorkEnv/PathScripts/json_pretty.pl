9bf925a9a49076b3237ff8ee06ea3194b05b7716
m <mmacnair.org>
2020-09-26 16:38:02 +0100

    missing tool

---
+++ WorkEnv/PathScripts/json_pretty.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use JSON;
+main( @ARGV );
+
+sub main {
+	my ( $file ) = @_;
+
+	unless ( index( lc( $file ), '.json' ) != -1 ) {
+		warn "\t[$file] is not a json file $/";
+		exit;
+	}
+	processfile( @_ );
+
+}
+
+sub processfile {
+
+	my $json = JSON->new();
+	$json->pretty( 1 );
+	$json->canonical( 1 );
+	my $buffer;
+	open( my $ifh, '<:raw', $_[0] ) or die $!;
+	while ( <$ifh> ) {
+		$buffer .= $_;
+	}
+	close $ifh;
+	my $href   = $json->decode( $buffer );
+	my $string = $json->encode( $href );
+	$string =~ s|   |\t|g;
+	open( my $ofh, '>:raw', $_[0] ) or die $!;
+	print $ofh $string;
+	close $ofh;
+
+}
