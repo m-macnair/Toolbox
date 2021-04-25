90613360312539575d13189746cd5a50ceeee31e
m <mmacnair.org>
2021-04-25 07:39:09 +0100

    1st week with slackware 15

---
+++ systems/slackware/15/removepkg/universal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Slackware packages I never use
+#Each letter indicates all packages starting with that have been checked
+#A
+removepkg aalib adwaita akregator alkimia amp
+#B
+removepkg blinken blueman bluedevil
