fe0b634b51af558ac53b4c5fe775fb9a7a50c30f
m <mmacnair.org>
2022-08-20 21:58:10 +0100

    added gpartted

--- systems/debian_like/shared_basic.sh
+++ systems/debian_like/shared_basic.sh
@@ -3,6 +3,6 @@
 echo "Running Shared Basic setup for Debian Derivatives";
 apt-get update --assume-yes
 apt-get upgrade --assume-yes
-apt-get install --assume-yes screen build-essential iptables git net-tools screen sudo  htop rsync perl ntpdate >> ~/apt_install.log
+apt-get install --assume-yes screen build-essential iptables git net-tools screen sudo  htop rsync perl ntpdate gparted >> ~/apt_install.log
 bash $TOOLBOXDIR/systems/cpan_universal.sh
 echo "/sbin/ is not added to path by default - ifconfig and several friends are to be found here!";
