e6141bc7fc29168e0011ba09bf605728d3f54367
m <mmacnair.org>
2023-07-19 17:47:59 +0100

    2023-07-18 reactive measures

--- systems/cpan/universal.sh
+++ systems/cpan/universal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#core used in every installation over which I have controller
-cpan  DBI CGI Moo Config::Any::Merge Data::UUID
+#core used in every installation over which I have control
+cpan  DBI CGI Moo Config::Any::Merge Data::UUID File::Find::Rule DBD::SQLite
